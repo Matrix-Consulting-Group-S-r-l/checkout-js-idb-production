4dec0413fcec52fede1508bcb5a1eb9690397ac4
Michele Selvaggio <m.selvaggio@matrixconsulting.it>
2025-05-05 10:43:25 +0200

    upd. 2025-05-05

--- packages/core/src/app/mtxConfig.ts
+++ packages/core/src/app/mtxConfig.ts
@@ -8,7 +8,7 @@ export const mtxConfig = {
         textCod: "Abbiamo ricevuto il tuo ordine. L'ordine è in fase di elaborazione nel nostro sistema. L'elaborazione potrebbe richiedere alcuni minuti.",
     },
     AddressCustomFields: {
-        pIvaID: "29", // Partiva IVA / Codice fiscale
-        fattID: "33", // Fatt
+        pIvaID: "30", // Partiva IVA / Codice fiscale   TEST: 29 - PROD: 30
+        fattID: "32", // Fatt   TEST: 33 - PROD: 32
     }
 }
